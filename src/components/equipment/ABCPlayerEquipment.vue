3f57127546b622d19975749619b8447a7c3d0f39
Isha Dijcks <ishadijcks@gmail.com>
2020-09-18 01:42:07 +0200

    Show stats in equipment

--- src/components/equipment/ABCPlayerEquipment.vue
+++ src/components/equipment/ABCPlayerEquipment.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <h4>Equipment</h4>
-    Weapon: <span v-if="equipment.equippedWeapon != null">{{ equipment.equippedWeapon.name }}</span>
+    Weapon: <span v-if="weapon != null">{{ weapon.name }}</span>
+    <button @click="unEquip('Weapon')">Unequip</button>
+    <ul>
+      <li>criticalChance: {{equipment.criticalChance}}</li>
+      <li>dodgeChance: {{equipment.dodgeChance}}</li>
+      <li>mageAttack: {{equipment.mageAttack}}</li>
+      <li>mageDefense: {{equipment.mageDefense}}</li>
+      <li>maxHealth: {{equipment.maxHealth}}</li>
+      <li>meleeAttack: {{equipment.meleeAttack}}</li>
+      <li>meleeDefense: {{equipment.meleeDefense}}</li>
+      <li>rangeAttack: {{equipment.rangeAttack}}</li>
+      <li>rangeDefense: {{equipment.rangeDefense}}</li>
+    </ul>
   </div>
 </template>
 
@@ -12,9 +24,20 @@ export default {
   name: "ABCPlayerEquipment",
   data() {
     return {
-      equipment: App.game.equipment
+      equipment: App.game.equipment,
     }
   },
+
+  methods: {
+    unEquip(type) {
+      this.equipment.unEquip(type)
+    }
+  },
+  computed: {
+    weapon() {
+      return this.equipment.equipment.Weapon
+    }
+  }
 
 }
 </script>
