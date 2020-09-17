fe026bd80edcda8a2073411934e3b112ed7d59f4
Isha Dijcks <ishadijcks@gmail.com>
2020-09-18 01:41:50 +0200

    Add more items

--- src/game/items/ItemList.ts
+++ src/game/items/ItemList.ts
@@ -5,6 +5,10 @@ import {MoneyPotion} from "@/game/items/items/MoneyPotion";
 import {EquipableInventory} from "@/game/items/EquipableInventory";
 import {Inventory} from "@/game/features/inventory/Inventory";
 import {InventoryId} from "@/game/features/inventory/InventoryId";
+import {Weapon} from "@/game/features/combat/Weapon";
+import {EquipmentType} from "@/game/features/equipment/EquipmentType";
+import {Attack} from "@/game/features/combat/Attack";
+import {WeaponType} from "@/game/features/combat/WeaponType";
 
 export class ItemList {
     static items: Record<ItemId, Item> = {} as Record<ItemId, Item>;
@@ -19,7 +23,18 @@ export class ItemList {
         this.registerItem(new Item("Ore 1", ItemId.Ore1, ItemType.Ore, 50));
         this.registerItem(new Item("Ore 2", ItemId.Ore2, ItemType.Ore, 50));
         this.registerItem(new Item("Bar 1", ItemId.Bar1, ItemType.Global, Infinity));
-        this.registerItem(new Item("Sword", ItemId.SomeSword, ItemType.Global, 1));
+        this.registerItem(new Weapon("Sword", ItemId.SomeSword, EquipmentType.Weapon,
+            {
+                meleeAttack: 10
+            },
+            [new Attack("Poke", WeaponType.Melee, 5, 10)])
+        );
+        this.registerItem(new Weapon("Sword2", ItemId.AnotherSword, EquipmentType.Weapon,
+            {
+                meleeAttack: 20
+            },
+            [new Attack("Poke2", WeaponType.Melee, 10, 15)])
+        );
     }
 
     static registerItem(item: Item) {
